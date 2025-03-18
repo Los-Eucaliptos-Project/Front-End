58dcdc7243a2b1b1a8e5edf7b035493445bb938b
a <sa>
2025-03-18 09:54:21 -0500

    fix: buttom in create new requirements

--- src/forms/pending-requirements-form/new-requirements-form/components/new-requirements-form-response-block-list.component.jsx
+++ src/forms/pending-requirements-form/new-requirements-form/components/new-requirements-form-response-block-list.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import NewRequirementsFormResponseBlock from './new-requirements-form-response-block.component'
-import styles from '../../../../modules/form-input-field.module.css'
+import NewRequirementsFormResponseBlock from './new-requirements-form-response-block.component';
+import styles from '../../../../modules/form-input-field.module.css';
 import CustomModalStyleButton from '../../../../components/custom-modal-components/custom-modal-style-buttoncomponent..jsx';
 
-
-function NewRequirementsFormResponseBlockList({label, bloques, onAgregar, onEliminar }) {
+function NewRequirementsFormResponseBlockList({ label, bloques, onAgregar, onEliminar }) {
   return (
-    <div style={{marginTop: '30px', textAlign: 'left'}}>
+    <div style={{ marginTop: '30px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
       <label className={styles.label}> {label}</label>
       {bloques.map((bloque) => (
         <NewRequirementsFormResponseBlock key={bloque.id} id={bloque.id} onEliminar={onEliminar} />
       ))}
-      <CustomModalStyleButton  onClick={onAgregar} primary={true} style={{ marginLeft: '-1100px' }}>Agregar</CustomModalStyleButton>
+      <div style={{alignSelf: 'flex-start', marginTop: '10px'}}>
+        <CustomModalStyleButton onClick={onAgregar} primary={true}>Agregar</CustomModalStyleButton>
+      </div>
     </div>
   );
 }
