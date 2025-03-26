eb2fe1a90b62b29408d66054e482149c70a5e9cb
a <sa>
2025-03-26 17:20:07 -0500

    fix: styles user name in module

--- src/components/page-header.component.jsx
+++ src/components/page-header.component.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import UserTextStyle from './user-text-style.component';
+import styles from '../modules/header-page.module.css';
 
-function PageHeader({ title, userName,  marginTop = '0' }) {
+function PageHeader({ title, userName, marginTop = '0' }) {
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginTop: marginTop,
-    }}>
-      <h1 style={{ fontSize: '1.7rem', fontWeight: '750', color: '#2b3e90' }}>{title}</h1>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: marginTop,
+      }}
+    >
+      <h1 style={{ fontSize: '1.7rem', fontWeight: '750', color: '#2b3e90' }}>
+        {title}
+      </h1>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <h3 style={UserTextStyle}>{userName}</h3>
+        <h3 className={styles.userTextStyle}>{userName}</h3>
       </div>
     </div>
   );
