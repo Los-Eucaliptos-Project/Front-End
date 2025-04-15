ba8527e37525229b884a33b78ec7df8a459f74d9
a <zarely.271217@gmail.com>
2025-04-15 10:17:30 -0500

    fix: slides notifications, pending requests, pending requirements, themes and users

--- src/layouts/slides/slide-notifications.layout.jsx
+++ src/layouts/slides/slide-notifications.layout.jsx
@@ -1,113 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import FormLogo from '../../components/form-logo.component';
-import SlideBarGreen from './slide-components/slide-side-bar-green.component.jsx';
-
+import React from 'react';
 import TemplateIcon from '../../components/icons/template-icon.icon.jsx';
 import icons from '../../utils/icons-library.js';
-
-import styles from '../../modules/slide.module.css'
-
-import { SlideCloseMenuIcon } from '../../styled/slide.styles.js';
-import { SlideItem } from '../../styled/slide.styles.js';
-import { SlideMenuSection } from '../../styled/slide.styles.js';
-import { SlideHamburgerMenu } from '../../styled/slide.styles.js';
-import { SlideLogoSection } from '../../styled/slide.styles.js';
-import { SlideLink } from '../../styled/slide.styles.js';
-import { SlideContainer } from '../../styled/slide.styles.js';
-
+import styles from '../../modules/slide.module.css';
+import { SlideCloseMenuIcon, SlideContainer } from '../../styled/slide.styles.js';
 import useHeaderSlide from '../../hooks/use-header-slide.hook';
 import usePageRefresh from '../../hooks/use-page-refresh.hook.jsx';
+import SlideMenu from './slide-components/slide-menu.component.jsx';
+import SlideFooter from './slide-components/slide-footer.component';
+import MenuItems from '../../utils/menu-items.js';
+import SlideHamburger from './slide-components/slide-hamburger.component.jsx';
+import SlideLogo from './slide-components/slide-logo.component.jsx';
+import SlideBarGreen from './slide-components/slide-side-bar-green.component.jsx';
 
 function SlideNotifications() {
+  const { isMobile, isMenuOpen, toggleMenu } = useHeaderSlide();
+  const handleRefresh = usePageRefresh('/notifications');
 
-    const { isMobile, isMenuOpen, toggleMenu } = useHeaderSlide();
-    const handleRefresh = usePageRefresh('/notifications');
+  const updatedMenuItems = MenuItems.map(item => {
+    if (item.href === '/notifications') {
+      return { ...item, onClick: () => handleRefresh('/notifications'), showGreenBar: true, greenBarMarginTop: '180px' };
+    }
+    return item;
+  });
 
   return (
-    <SlideContainer isMobile={isMobile}>
+    <SlideContainer $isMobile={isMobile}>
       <div className={styles.slideContent}>
-        <SlideLogoSection isMenuOpen={isMenuOpen}>
-          <SlideLink href="/">
-            <FormLogo width="200px" height="80px" marginLeft="-1px" />
-          </SlideLink>
-        </SlideLogoSection>
+        <SlideLogo isMenuOpen={isMenuOpen} />
+        <SlideHamburger toggleMenu={toggleMenu} />
 
-        <SlideHamburgerMenu onClick={toggleMenu}>
-          <TemplateIcon icon={icons.faBars} />
-        </SlideHamburgerMenu>
+        {isMenuOpen && (
+          <SlideCloseMenuIcon onClick={toggleMenu}>
+            <TemplateIcon icon={icons.faTimes} />
+          </SlideCloseMenuIcon>
+        )}
 
-         {isMenuOpen && (
-            <SlideCloseMenuIcon onClick={toggleMenu}>
-              <TemplateIcon icon={icons.faTimes} />
-            </SlideCloseMenuIcon>
-          )}      
-
-        <SlideMenuSection isMobile={isMobile} isMenuOpen={isMenuOpen}>
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/dashboard">
-              <TemplateIcon icon={icons.faThLarge} style={{ marginRight: '0.7rem' }} />
-              Dashboard
-            </SlideLink>
-          </SlideItem>
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/pending-requirements">
-              <TemplateIcon icon={icons.faTag} style={{ marginRight: '0.7rem' }} />
-              Req. Pendientes
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/closed-requirements">
-              <TemplateIcon icon={icons.faTags}  style={{ marginRight: '0.7rem' }} />
-              Req. Cerrados
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/pending-requests">
-              <TemplateIcon icon={icons.faScrewdriverWrench} style={{ marginRight: '0.7rem' }} />
-              Sol. Pendientes
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/closed-requests">
-              <TemplateIcon icon={icons.faCheck} style={{ marginRight: '0.7rem' }} />
-              Sol. Cerrados
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/themes">
-              <TemplateIcon icon={icons.faBarsProgress} style={{ marginRight: '0.7rem' }} />
-              Temas
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/users">
-              <TemplateIcon icon={icons.faUser} style={{ marginRight: '0.7rem' }} />
-              Usuarios
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <div className={styles.slideSelectButton} href="/notifications" onClick={handleRefresh}>
-              <TemplateIcon icon={icons.faBell} style={{ marginRight: '0.7rem' }} />
-              Notificaciones
-            </div>
-            <SlideBarGreen marginTop="660px" />
-          </SlideItem>
-
-          <div className = {styles.slideFooterContainer}>
-            <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen} className="logout-item">
-              <SlideLink href="/login">
-                <TemplateIcon icon={icons.faPowerOff} style={{ marginRight: '0.5rem' }} />
-                Salir
-              </SlideLink>
-            </SlideItem>
-          </div>
-        </SlideMenuSection>
+        <SlideMenu isMobile={isMobile} isMenuOpen={isMenuOpen} handleRefresh={handleRefresh} menuItems={updatedMenuItems} selectedPath="/notifications" />
+        <SlideBarGreen marginTop="660px" />
+  |     <SlideFooter isMobile={isMobile} isMenuOpen={isMenuOpen} />
       </div>
     </SlideContainer>
   );
--- src/layouts/slides/slide-pending-requirements.layout.jsx
+++ src/layouts/slides/slide-pending-requirements.layout.jsx
@@ -1,117 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import FormLogo from '../../components/form-logo.component';
-import SlideBarGreen from './slide-components/slide-side-bar-green.component.jsx';
-
+import React from 'react';
 import TemplateIcon from '../../components/icons/template-icon.icon.jsx';
 import icons from '../../utils/icons-library.js';
-
-import styles from '../../modules/slide.module.css'
-
-import { SlideCloseMenuIcon } from '../../styled/slide.styles.js';
-import { SlideItem } from '../../styled/slide.styles.js';
-import { SlideMenuSection } from '../../styled/slide.styles.js';
-import { SlideHamburgerMenu } from '../../styled/slide.styles.js';
-import { SlideLogoSection } from '../../styled/slide.styles.js';
-import { SlideLink } from '../../styled/slide.styles.js';
-import { SlideContainer } from '../../styled/slide.styles.js';
-
+import styles from '../../modules/slide.module.css';
+import { SlideCloseMenuIcon, SlideContainer } from '../../styled/slide.styles.js';
 import useHeaderSlide from '../../hooks/use-header-slide.hook';
 import usePageRefresh from '../../hooks/use-page-refresh.hook.jsx';
+import SlideMenu from './slide-components/slide-menu.component.jsx';
+import SlideFooter from './slide-components/slide-footer.component';
+import MenuItems from '../../utils/menu-items.js';
+import SlideHamburger from './slide-components/slide-hamburger.component.jsx';
+import SlideLogo from './slide-components/slide-logo.component.jsx';
+import SlideBarGreen from './slide-components/slide-side-bar-green.component.jsx';
 
-function SlidePendingRequirements() {
+function SlideDashboard() {
+  const { isMobile, isMenuOpen, toggleMenu } = useHeaderSlide();
+  const handleRefresh = usePageRefresh('/pending-requirements');
 
-    const { isMobile, isMenuOpen, toggleMenu } = useHeaderSlide();
-    const handleRefresh = usePageRefresh('/pending-requirements');
+  const updatedMenuItems = MenuItems.map(item => {
+    if (item.href === '/pending-requirements') {
+      return { ...item, onClick: () => handleRefresh('/pending-requirements'), showGreenBar: true, greenBarMarginTop: '180px' };
+    }
+    return item;
+  });
 
   return (
-    <SlideContainer isMobile={isMobile}>
+    <SlideContainer $isMobile={isMobile}>
       <div className={styles.slideContent}>
-        <SlideLogoSection isMenuOpen={isMenuOpen}>
-          <SlideLink href="/">
-            <FormLogo width="200px" height="80px" marginLeft="-1px" />
-          </SlideLink>
-        </SlideLogoSection>
+        <SlideLogo isMenuOpen={isMenuOpen} />
+        <SlideHamburger toggleMenu={toggleMenu} />
 
-        <SlideHamburgerMenu onClick={toggleMenu}>
-          <TemplateIcon icon={icons.faBars} />
-        </SlideHamburgerMenu>
+        {isMenuOpen && (
+          <SlideCloseMenuIcon onClick={toggleMenu}>
+            <TemplateIcon icon={icons.faTimes} />
+          </SlideCloseMenuIcon>
+        )}
 
-         {isMenuOpen && (
-            <SlideCloseMenuIcon onClick={toggleMenu}>
-              <TemplateIcon icon={icons.faTimes} />
-            </SlideCloseMenuIcon>
-          )}    
-
-        <SlideMenuSection isMobile={isMobile} isMenuOpen={isMenuOpen}>
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/dashboard">
-              <TemplateIcon icon={icons.faThLarge} style={{ marginRight: '0.7rem' }} />
-              Dashboard
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <div className={styles.slideSelectButton} href="/pending-requirements" onClick={handleRefresh}>
-              <TemplateIcon icon={icons.faTag} style={{ marginRight: '0.7rem' }} />
-              Req. Pendientes
-            </div>
-            <SlideBarGreen marginTop="240px" />
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/closed-requirements">
-              <TemplateIcon icon={icons.faTags}  style={{ marginRight: '0.7rem' }} />
-              Req. Cerrados
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/pending-requests">
-              <TemplateIcon icon={icons.faScrewdriverWrench} style={{ marginRight: '0.7rem' }} />
-              Sol. Pendientes
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/closed-requests">
-              <TemplateIcon icon={icons.faCheck} style={{ marginRight: '0.7rem' }} />
-              Sol. Cerrados
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/themes">
-              <TemplateIcon icon={icons.faBarsProgress} style={{ marginRight: '0.7rem' }} />
-              Temas
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/users">
-              <TemplateIcon icon={icons.faUser} style={{ marginRight: '0.7rem' }} />
-              Usuarios
-            </SlideLink>
-          </SlideItem>
-
-          <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen}>
-            <SlideLink href="/notifications">
-              <TemplateIcon icon={icons.faBell} style={{ marginRight: '0.7rem' }} />
-              Notificaciones
-            </SlideLink>
-          </SlideItem>
-
-          <div className = {styles.slideFooterContainer}>
-            <SlideItem isMobile={isMobile} isMenuOpen={isMenuOpen} className="logout-item">
-              <SlideLink href="/login">
-                <TemplateIcon icon={icons.faPowerOff} style={{ marginRight: '0.5rem' }} />
-                Salir
-              </SlideLink>
-            </SlideItem>
-          </div>
-        </SlideMenuSection>
+        <SlideMenu isMobile={isMobile} isMenuOpen={isMenuOpen} handleRefresh={handleRefresh} menuItems={updatedMenuItems} selectedPath="/pending-requirements" />
+        <SlideBarGreen marginTop="240px" />
+  |     <SlideFooter isMobile={isMobile} isMenuOpen={isMenuOpen} />
       </div>
     </SlideContainer>
   );
 }
 
-export default SlidePendingRequirements;
+export default SlideDashboard;
